c5ae19e82029a09b16efaf56f3dc648499290e53
andrenunesrocha <78302756+andrenunesrocha@users.noreply.github.com>
2021-01-31 09:12:29 -0300

    Add files via upload

---
+++ folha/src/test/java/br/com/folha/folha/FolhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.folha.folha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
